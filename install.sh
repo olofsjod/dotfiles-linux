eaaab1c483b660a6d3c73ba6ef61d8f35e43e8f5
Olof Sjödin <me@olofsjodin.se>
2015-04-10 04:57:42 +0200

    added addlink, install, uninstall, rmlink functions. appended GPL v3 licence

--- install.sh
+++ install.sh
@@ -1,36 +1,105 @@
 #!/usr/bin/env bash
 
+  #########################
+## dotfiles install script ##
+## by medik         v0.0.1 ##
+  #########################
+
+# Wrote this as a practice to make good and robust
+# bash scripts. 
+
+# Copyright (C) 2015  Olof Sjödin
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+echo -e "dotfiles installation script v0.0.1 \n"
+echo "Copyright (C) 2015  Olof Sjödin"
+echo "This program comes with ABSOLUTELY NO WARRANTY."
+echo -e "This is free software, and you are welcome to redistribute it under certain conditions.\n"
+
 cur_path=`pwd`
 
-echo "Initializing submodules"
-git submodule init
-git submodule update
+function addlink {
+    echo "Adding link to $1"
+    t="$cur_path/$1"
+    f="$HOME/.$1"
+    if [[ ! -h $f ]]; then
+        if [[ -e $f || -d $f ]]; then
+            backup="$f"
+            backup+=".bak"
+            echo Backuping existing file or directory to $backup
+            mv -v $f $backup
+        fi
+        ln -sv $t $f
+    fi
+}
 
-# Prepare for cowbells
-mkdir -p $cur_path/vim/{colors,autoload}
+function rmlink {
+    t="$cur_path/$1"
+    f="$HOME/.$1"
+    if [[ -h $f ]]; then
+        rm -v $f
+    fi
+}
 
-echo "Deleting the old symlinks"
-rm -f {~/.bash_profile,~/.bashrc,~/.vimrc,~/.vim,~/.gvimrc,~/.gitconfig,\
-~/.gitignore,~/.githelpers,~/.Xresources,~/.conkyrc,~/.aliases,~/.git-completion.bash,~/.zshrc}
+function uninstall {
+    echo "Running uninstall script"
+    echo "Will try to remove any links that are in $HOME"
+    echo "Are you sure? (y/N)"
 
-echo "Symlinking files"
-ln -s $cur_path/aliases ~/.aliases
-ln -s $cur_path/bash_profile ~/.bash_profile
-ln -s $cur_path/bashrc ~/.bashrc
-ln -s $cur_path/vimrc ~/.vimrc
-ln -s $cur_path/vim ~/.vim
-ln -s $cur_path/gvimrc ~/.gvimrc
-ln -s $cur_path/gitconfig ~/.gitconfig
-ln -s $cur_path/gitignore ~/.gitignore
-ln -s $cur_path/githelpers ~/.githelpers
-ln -s $cur_path/conkyrc ~/.conkyrc
-ln -s $cur_path/Xresources ~/.Xresources
-ln -s $cur_path/git-completion.bash ~/.git-completion.bash
-ln -s $cur_path/zshrc ~/.zshrc
+    rmlink bash_profile
+    rmlink bashrc
+    rmlink vimrc
+    rmlink vim
+    rmlink gvimrc
+    rmlink gitconfig
+    rmlink gitignore
+    rmlink githelpers
+    rmlink Xresources
+    rmlink conkyrc
+    rmlink aliases
+    rmlink git-completion.bash
+    rmlink zshrc
+}
 
-curl -s https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o $cur_path/git-completion.bash
+function install {
+    echo "Running install script"
+    mkdir -p $cur_path/vim/{colors,autoload}
 
-echo "Installing plugins to vim"
-vim +PluginInstall +qall > /dev/null 2>&1
+    echo "Initializing git submodules"
+    git submodule init
+    git submodule update
+
+    curl -s https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o $cur_path/git-completion.bash
+
+    addlink bash_profile
+    addlink bashrc
+    addlink vimrc
+    addlink vim
+    addlink gvimrc
+    addlink gitconfig
+    addlink gitignore
+    addlink githelpers
+    addlink Xresources
+    addlink conkyrc
+    addlink aliases
+    addlink git-completion.bash
+    addlink zshrc
+
+    echo "Installing silently plugins to vim"
+    vim +PluginInstall +qall > /dev/null 2>&1
+}
+
 
 echo "All done."
