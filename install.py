4d85b24aa1e51acc84084cc62658c9d6a5af0097
Olof Sjödin <olsj@kth.se>
2023-03-29 10:35:56 +0200

    fix broken emacs, scrot selects now, cleanup install.py

--- install.py
+++ install.py
@@ -3,7 +3,6 @@ import sys
 import shutil
 import getpass
 
-from optparse import OptionParser
 from subprocess import Popen, PIPE
 
 def copy_config_files():
@@ -14,12 +13,13 @@ def copy_config_files():
 
     src = os.path.join(os.getcwd(), confdir)
     dst = os.getenv("HOME")
-    
+
     for n in names:
         srcname = os.path.join(src, n)
         dstname = os.path.join(dst, "." + n)
 
         try:
+            # Check whether file or directory exists
             if not os.path.isfile(dstname) and not os.path.isdir(dstname):
                 if os.path.isdir(srcname):
                     shutil.copytree(srcname, dstname)
@@ -27,64 +27,12 @@ def copy_config_files():
                     shutil.copy2(srcname, dstname)
             else:
                 print("Skipping " + n)
-                
+
             #shutil.copystat(src, dst)
         except:
             print("Unexpected error:", sys.exc_info()[0])
 
 
-def create_dnf_install_str():
-    prg = ["openafs-client",
-           "heimdal-workstation",
-           "i3",
-           "i3blocks",
-           "i3lock",
-           "i3status",
-           "rofi",
-           "conky",
-           "redshift",
-           "mpd",
-           "dunst",
-           "pasystray",
-           "ncmpcpp",
-           "feh",
-           "texlive-scheme-full",
-           "fwupd",
-           "emacs"]
-
-    cmd = "dnf install -y "
-    for p in prg:
-        cmd += p + " "
-
-    return cmd
-
 if __name__ == "__main__":
-    print("medik's install script as of 2018-04-14 for Fedora Linux\n")
-    parser = OptionParser()
-    parser.add_option("-s", "--install-sw", action="store_true", default=False,
-                      help="Install software with dnf", dest="software_install")
-    parser.add_option("-c", "--install-configs",
-                      action="store_true", default=False,
-                      help="Install configs", dest="configs_install")
-
-    (options, args) = parser.parse_args()
-
-    if options.software_install:
-        print("Installing default software")
-        print("Please enter your sudo password")
-        sudo_pw = getpass.getpass()
-        
-        cmd = create_dnf_install_str()
-        command = cmd.split(" ")
-        
-        p = Popen(['sudo', '-S'] + command, stdin=PIPE, stderr=PIPE,
-          universal_newlines=True)
-        sudo_prompt = p.communicate(sudo_pw + '\n')[1]
-        print(sudo_prompt)
-        
-
-    if options.configs_install:
-        print("Install configs")        
-        copy_config_files()
-
-    
+    print("Install configs")
+    copy_config_files()
